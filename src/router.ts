1ee9997c22349d3ffefb959dc29827e88a21932b
a-hirata <hirataakira6@gmail.com>
2019-08-04 17:03:23 +0900

    test multi page

--- src/router.ts
+++ src/router.ts
@@ -9,12 +9,12 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/app/',
       name: 'home',
       component: Home,
     },
     {
-      path: '/about',
+      path: '/app/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
