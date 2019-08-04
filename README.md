333a80ff46f67a715ccedafb14fe45d83cf3d7e1
a-hirata <hirataakira6@gmail.com>
2019-08-04 14:36:08 +0900

    edit basic status

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # iaia
+TRPG Support App for Call of Cthulfu.  
+URL: [iaia.app](http://iaia.app/)
+
+# Command
+Action | Command
+--|--
+Start Dev | `$ npm start`
+Unit Test | `$ npm test:unit`
+E2E Test | `$ npm test:e2e`
+Build | `$ npm runn Build`
+
+# Directory Info
+Directory | Puropose
+--|--
+`/src` | source files
+`/docs` | Release files for GitHub Pages 
+`/design` | designn files
+`/public` | Static files
---
+++ docs/precache-manifest.f22f5a0732250f34943471e18f2544eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8c5eafb6f6fffaae5460",
+    "url": "/js/chunk-vendors.c96832ad.js"
+  },
+  {
+    "revision": "812ebb4e3bdeb506d31b",
+    "url": "/js/app.62081158.js"
+  },
+  {
+    "revision": "2f820f1059b79a741cdf",
+    "url": "/js/about.684d4714.js"
+  },
+  {
+    "revision": "4c32631246b984cd80fbfc0b6021d8c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "812ebb4e3bdeb506d31b",
+    "url": "/css/app.1b110378.css"
+  }
+];
