5b5d1d33db5fdb103e6b4f0357ad9fe7d727c953
dulltz <isrgnoe@gmail.com>
2019-08-10 18:45:32 +0900

    Enable firebase authentication

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,31 +1,26 @@
 <template><div>
-
-  <main>
-    <h1>Login</h1>
-    <button @click="$store.commit('login')">LOGIN</button>
-    <router-link to="/dice">ログインせずに試す</router-link>
-  </main>
-
+  <Home v-if="!$store.state.login"></Home>
 </div></template>
-
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Home from '@/components/Home.vue';
 
 @Component({
   components: {
+      Home,
   },
 })
+
 export default class Login extends Vue {
   // lifecycle hook
   public beforeCreate() {
-    if (this.$store.state.login) { // ログイン済みの場合
-      this.$router.push('/dice');
-    }
+    // if (this.$store.state.login) { // ログイン済みの場合
+    //   this.$router.push('/dice');
+    // }
   }
 }
 </script>
-
 
 <style scoped lang="scss">
 @import "@/scss/common.scss";
