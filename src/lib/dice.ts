eff4f0e62865020dcae74dce105937501d238773
dulltz <isrgnoe@gmail.com>
2019-09-16 16:30:54 +0900

    Add unit test for dice func

--- src/lib/dice.ts
+++ src/lib/dice.ts
@@ -1,3 +1,7 @@
+import MersenneTwister from 'mersenne-twister';
+
+const generator = new MersenneTwister();
+
 export function throwDice(nDice: number, maxNumber: number): number {
     if (maxNumber === 100) {
         return nDice * getRandomInt(1, 99);
@@ -5,9 +9,6 @@ export function throwDice(nDice: number, maxNumber: number): number {
     return nDice * getRandomInt(1, maxNumber);
 }
 
-function getRandomInt(min: number, max: number) {       
-    const byteArray = new Uint8Array(1);
-    window.crypto.getRandomValues(byteArray);
-    const randomNum = '0.' + byteArray[0].toString();
-    return Math.floor(Number(randomNum) * (max - min + 1)) + min;
+function getRandomInt(min: number, max: number) {
+  return generator.random_int() % (max - min) + min + 1;
 }
