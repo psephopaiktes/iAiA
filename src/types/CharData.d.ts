811d48a11184f1a3863180d765c174758df74115
a-hirata <hirataakira6@gmail.com>
2020-03-29 16:01:20 +0900

    fix by review

--- src/types/CharData.d.ts
+++ src/types/CharData.d.ts
@@ -4,8 +4,8 @@ import DocumentReference = firebase.firestore.DocumentReference;
 export default interface CharData {
   id?: string; // キャラクターの一意のID
   user?: string; // キャラクター作成者
-  createDate?: number; // 作成日 経過ミリ秒の文字列でいいのかな？
-  modifiedDate?: string; // 更新日 キャラ一覧の並び替えなどで使う用
+  createDate?: Date; // 作成日 経過ミリ秒の文字列でいいのかな？
+  modifiedDate?: Date; // 更新日 キャラ一覧の並び替えなどで使う用
   userRef?: DocumentReference; //?
   profile?: {
     // キャラクターの基本情報
@@ -14,20 +14,21 @@ export default interface CharData {
     avatarUrl?: string; // プロフィール画像のURL
     occupation?: string; // 職業
     backstory?: string; // 説明文 改行を許容
-    height?: number; // 身長
+    heightMeter?: number; // 身長
     age?: number;
     weight?: number;
-    sex?: string; // 性別 [male|female]
-    nationality?: string; // 出身
+    sex?: string; // 性別
+    nationality?: string; // 国籍
     money?: number; // 所持金
-    // weapon?: [ // 武器
+    moneyUnit?: number; // 所持金の単位
+    // weapons?: [ // 武器
     //   {},
     // ],
     // belongings?: [ // 所持品
     //   {},
     // ],
   };
-  characteristics?: {
+  ability?: {
     // キャラクターの能力値 ダイスで決まるもの
     STR?: number;
     CON?: number;
